19a77889cb7663dfb76bba6b961eac37b4def6bc
auysh8 <auysh1993@gmail.com>
2026-05-15 23:48:29 +0530

    feat: create Dashboard component with layout and note management functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,116 +1,15 @@
 import { useState } from "react";
-import Sidebar from "./components/Sidebar/Sidebar";
-import SearchBar from "./components/Search bar/SearchBar";
 import "./App.css";
-import NoteGrid from "./components/Notes Grid/NotesGrid";
-import NoteEdit from "./components/Note Edit View/NoteEdit";
-import { useNavigate, useLocation } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
-import type { Note } from "./types";
-import { useNotes } from "./hooks/useNotes";
 import AuthPage from "./pages/AuthPage";
+import Dashboard from "./dashboard/Dashboard";
 
 const App = () => {
   const [isAuthView, setIsAuthView] = useState(true);
-  const [searchInput, setSearchInput] = useState("");
-  const navigate = useNavigate();
-  const location = useLocation();
-  const {
-    notes,
-    isNoteView,
-    selectNote,
-    handlePermanentlyDelete,
-    handleNoteRestore,
-    handleTrashNote,
-    handleArchiveNote,
-    handlePinNote,
-    newNoteSave,
-    editNoteSave,
-    handleNewNote,
-    handleNote,
-    noteClose,
-  } = useNotes();
 
-  const activeTab =
-    location.pathname === "/" ? "notes" : location.pathname.substring(1);
-
-  const handleSearchBar = (input: string) => {
-    setSearchInput(input);
-  };
-
-  const handleSidebar = (tab: string) => {
-    if (tab === "notes") {
-      navigate("/");
-    } else {
-      navigate(`/${tab}`);
-    }
-  };
-
-  const renderNotes = () => {
-    let filterNotes: Note[] = [];
-    if (activeTab === "notes") {
-      filterNotes = notes.filter((note) => !note.isArchived && !note.isTrashed);
-    } else if (activeTab === "archive") {
-      filterNotes = notes.filter((note) => note.isArchived && !note.isTrashed);
-    } else if (activeTab === "trash") {
-      filterNotes = notes.filter((note) => note.isTrashed);
-    }
-
-    if (searchInput.trim().length > 0) {
-      filterNotes = filterNotes.filter((note) => {
-        return (
-          note.title?.toLowerCase().includes(searchInput) ||
-          note.content?.toLowerCase().includes(searchInput)
-        );
-      });
-    }
-
-    filterNotes = filterNotes.sort((a, b) => {
-      if (b.isPinned !== a.isPinned) {
-        return Number(b.isPinned || 0) - Number(a.isPinned || 0);
-      }
-      return (
-        new Date(b.lastEdited).getTime() - new Date(a.lastEdited).getTime()
-      );
-    });
-    return filterNotes;
-  };
   if (isAuthView) {
     return <AuthPage onLogin={() => setIsAuthView(false)} />;
   } else {
-    return (
-      <div className="app">
-        <Sidebar
-          onClick={handleSidebar}
-          tab={activeTab}
-          newNote={handleNewNote}
-        />
-        <div className="dashboard">
-          <SearchBar input={handleSearchBar} />
-          <NoteGrid
-            tab={activeTab}
-            onClick={handleNote}
-            notes={renderNotes()}
-            pinId={handlePinNote}
-            archiveId={handleArchiveNote}
-            trashId={handleTrashNote}
-            restoreId={handleNoteRestore}
-            deleteId={handlePermanentlyDelete}
-          />
-        </div>
-        <AnimatePresence>
-          {isNoteView && (
-            <NoteEdit
-              layoutId={selectNote}
-              onOpen={selectNote}
-              onClose={noteClose}
-              onNewSave={newNoteSave}
-              onEditedSave={editNoteSave}
-            />
-          )}
-        </AnimatePresence>
-      </div>
-    );
+    return <Dashboard />;
   }
 };
 
---
+++ src/dashboard/Dashboard.tsx
@@ -0,0 +1,110 @@
+import { useState } from "react";
+import Sidebar from "../components/Sidebar/Sidebar";
+import SearchBar from "../components/Search bar/SearchBar";
+import styles from "./Dashboard.module.css"
+import NoteGrid from "../components/Notes Grid/NotesGrid";
+import NoteEdit from "../components/Note Edit View/NoteEdit";
+import { useNavigate, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import type { Note } from "../types";
+import { useNotes } from "../hooks/useNotes";
+const Dashboard = () => {
+  const [searchInput, setSearchInput] = useState("");
+  const navigate = useNavigate();
+  const location = useLocation();
+  const {
+    notes,
+    isNoteView,
+    selectNote,
+    handlePermanentlyDelete,
+    handleNoteRestore,
+    handleTrashNote,
+    handleArchiveNote,
+    handlePinNote,
+    newNoteSave,
+    editNoteSave,
+    handleNewNote,
+    handleNote,
+    noteClose,
+  } = useNotes();
+
+  const activeTab =
+    location.pathname === "/" ? "notes" : location.pathname.substring(1);
+
+  const handleSearchBar = (input: string) => {
+    setSearchInput(input);
+  };
+
+  const handleSidebar = (tab: string) => {
+    if (tab === "notes") {
+      navigate("/");
+    } else {
+      navigate(`/${tab}`);
+    }
+  };
+
+  const renderNotes = () => {
+    let filterNotes: Note[] = [];
+    if (activeTab === "notes") {
+      filterNotes = notes.filter((note) => !note.isArchived && !note.isTrashed);
+    } else if (activeTab === "archive") {
+      filterNotes = notes.filter((note) => note.isArchived && !note.isTrashed);
+    } else if (activeTab === "trash") {
+      filterNotes = notes.filter((note) => note.isTrashed);
+    }
+
+    if (searchInput.trim().length > 0) {
+      filterNotes = filterNotes.filter((note) => {
+        return (
+          note.title?.toLowerCase().includes(searchInput) ||
+          note.content?.toLowerCase().includes(searchInput)
+        );
+      });
+    }
+
+    filterNotes = filterNotes.sort((a, b) => {
+      if (b.isPinned !== a.isPinned) {
+        return Number(b.isPinned || 0) - Number(a.isPinned || 0);
+      }
+      return (
+        new Date(b.lastEdited).getTime() - new Date(a.lastEdited).getTime()
+      );
+    });
+    return filterNotes;
+  };
+  return (
+    <div className={styles.app}>
+      <Sidebar
+        onClick={handleSidebar}
+        tab={activeTab}
+        newNote={handleNewNote}
+      />
+      <div className={styles.dashboard}>
+        <SearchBar input={handleSearchBar} />
+        <NoteGrid
+          tab={activeTab}
+          onClick={handleNote}
+          notes={renderNotes()}
+          pinId={handlePinNote}
+          archiveId={handleArchiveNote}
+          trashId={handleTrashNote}
+          restoreId={handleNoteRestore}
+          deleteId={handlePermanentlyDelete}
+        />
+      </div>
+      <AnimatePresence>
+        {isNoteView && (
+          <NoteEdit
+            layoutId={selectNote}
+            onOpen={selectNote}
+            onClose={noteClose}
+            onNewSave={newNoteSave}
+            onEditedSave={editNoteSave}
+          />
+        )}
+      </AnimatePresence>
+    </div>
+  );
+};
+
+export default Dashboard;
